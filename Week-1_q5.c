89ab55cfbd70f916a0b2c6288fcc4a9bdbca1e9e
Swayam792 <77006371+Swayam792@users.noreply.github.com>
2021-02-06 18:40:10 +0530

    Update Week-1_q5.c

--- Week-1_q5.c
+++ Week-1_q5.c
@@ -21,6 +21,21 @@ int main()
     case 'u' :
        printf("This is vowel");
        break;
+    case 'A' :
+      printf("This is vowel");
+      break;
+    case 'E' :
+      printf("This is vowel");
+      break;
+    case 'I' :
+      printf("This is vowel");
+      break;
+    case 'O' :
+      printf("This is vowel");
+      break;
+    case 'U' :
+      printf("This is vowel");
+      break;
      default :
       
        printf("This is consonant");
